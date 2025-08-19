8f636398ee8b018b8a26b99089afbf4f540062c6
FabianAJ04 <150567843+FabianAJ04@users.noreply.github.com>
2025-08-19 17:53:50 -0600

    Se agrega ShipmentViewModel

--- ReportesDePaqueteria/MVVM/ViewModels/SignInViewModel.cs
+++ ReportesDePaqueteria/MVVM/ViewModels/SignInViewModel.cs
@@ -1,58 +1,148 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
-using Firebase.Auth;
+
 
 namespace ReportesDePaqueteria.MVVM.ViewModels
 {
 
-    public partial class SignInViewModel : ObservableObject
+    public class ShipmentViewModel
     {
-        private readonly FirebaseAuthClient _authClient;
+        private readonly ShipmentRepository _repository; // Repositorio para manejar los envíos
+        public ShipmentModel Shipment { get; set; } = new ShipmentModel(); // Objeto para crear o editar un envío
+        public ObservableCollection<ShipmentModel> lstShipments { get; set; } = 
+        new ObservableCollection<ShipmentModel>(); // Colección observable para la interfaz de usuario
 
-        [ObservableProperty]
-        private string _email;
+        // Comandos
+        public ICommand CreateCommand { get; set; }
+        public ICommand LoadCommand { get; set; }
+        public ICommand DeleteCommand { get; set; }
+        public ICommand EditCommand { get; set; }
 
-        [ObservableProperty]
-        private string _password;
+        public INavigation _navigation { get; set; } // Navegación para cambiar de vistas
 
-        public SignInViewModel(FirebaseAuthClient authClient)
+        public ShipmentsViewModel(INavigation navigation)
         {
-            _authClient = authClient;
+            _repository = new ShipmentsRepository();
+            CreateCommand = new Command(async () => await CrearShipment());
+            LoadCommand = new Command(async () => await LoadShipment());
+            DeleteCommand = new Command<String>(async (Code) => await DeleteShipment(Code));
+            EditCommand = new Command(async () => await UpdateShipment(Shipment));
+            _navigation = navigation;
         }
 
-        [RelayCommand]
-        private async Task SignIn()
+
+        private async Task UpdateShipment(ShipmentModel newShipment)
         {
             try
             {
-                var authResult = await _authClient.SignInWithEmailAndPasswordAsync(Email, Password);
-                var userId = authResult.User.Uid;
-
-
-                await SecureStorage.SetAsync("user_id", userId);
-
-                // Buscar al usuario en Realtime DB para redirección
-                var userRepo = new UserRepository();
-                // Verificar existencia del usuario en DB y obtener datos adicionales
-                var usuario = await userRepo.GetUserById(userId);
-
-                if (usuario == null)
+                if (Validar())
                 {
-                    await Shell.Current.DisplayAlert("Error", "Usuario no registrado en la base de datos.", "OK");
-                    return;
+                    await _repository.UpdateDocumentAsync(newShipment, newShipment.Code);
+                    await ShowMessage("Envio modificado exitosamente!", true);
+                    Console.WriteLine("Envio modificado exitosamente!");
+                    _navigation.PopAsync();
+                    Clean();
                 }
             }
             catch (Exception ex)
             {
-                await Shell.Current.DisplayAlert("Error", "Error al iniciar sesión: ", "OK");
-                return;
+                Console.WriteLine($"Ocurrio un error al modificar el envio. Error: {ex.ToString()}");
+            }
+
+        }
+
+        private async Task DeleteShipment(string Code)
+        {
+            try
+            {
+                bool isComfirmed = await App.Current.MainPage.DisplayAlert(
+                    "Alerta",
+                    "Desar borrar el envio?",
+                    "Si",
+                    "No");
+
+                if (!isComfirmed) return;
+
+                await _repository.DeleteDocumentAsync(Code);
+                await LoadShipment();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Ocurrio un error al borrar el envio. Error: {ex.ToString()}");
             }
         }
 
-            [RelayCommand]
-        private async Task NavigateSignUp()
+        private async Task LoadShipment()
         {
-            await Shell.Current.GoToAsync("//SignUp");
+            try
+            {
+                var ShipmentsFromDB = await _repository.GetAllAsync();
+
+                lstShipments.Clear();
+
+                foreach (var Shipment in ShipmentsFromDB)
+                {
+                    lstShipments.Add(new ShipmentModel
+                    {
+                        Code = Shipment.Code,
+                        Nombre = Shipment.Value.Nombre,
+                        Telefono = Shipment.Value.Telefono,
+                        Correo = Shipment.Value.Correo,
+                        Direccion = Shipment.Value.Direccion
+                    });
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Ocurrio un error al cargar los envios. Error: {ex.ToString()}");
+            }
+        }
+
+        private async Task CrearShipment()
+        {
+            try
+            {
+                if (Validar())
+                {
+                    await _repository.CreateDocumentAsync(Shipment);
+                    await ShowMessage("Envio agregado exitosamente!", true);
+                    Console.WriteLine("Envio agregado exitosamente!");
+                    _navigation.PopAsync();
+                    Clean();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Ocurrio un error al crear el envio. Error: {ex.ToString()}");
+            }
+
+        }
+
+        private bool Validar()
+        {
+            bool respuesta = true;
+            if (string.IsNullOrWhiteSpace(Shipment.Nombre) ||
+               string.IsNullOrWhiteSpace(Shipment.Telefono) ||
+               string.IsNullOrWhiteSpace(Shipment.Correo) ||
+               string.IsNullOrWhiteSpace(Shipment.Direccion))
+            {
+                ShowMessage("Por favor completar todos los campos", false);
+                respuesta = false;
+            }
+            return respuesta;
+        }
+
+        private void Clean()
+        {
+            Shipment = new ShipmentModel();
+        }
+
+        private async Task ShowMessage(string message, bool isSuccess)
+        {
+            await App.Current.MainPage.DisplayAlert(
+                    isSuccess ? "Informacíon" : "Error",
+                    message,
+                    "Ok");
         }
     }
 }
