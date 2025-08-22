d55e7e9d989af90fda7cfdf9a3280299f9a00170
Tinnged <114170587+Tinnged@users.noreply.github.com>
2025-08-22 16:28:13 -0600

    Se agrega nav bar y se remueve banner detallado por simplicidad

--- ReportesDePaqueteria/AppShell.xaml.cs
+++ ReportesDePaqueteria/AppShell.xaml.cs
@@ -9,14 +9,20 @@ namespace ReportesDePaqueteria
             InitializeComponent();
             Routing.RegisterRoute(nameof(ShipmentDetailPage), typeof(ShipmentDetailPage));
             Routing.RegisterRoute(nameof(ShipmentFormPage), typeof(ShipmentFormPage));
+            Routing.RegisterRoute(nameof(ShipmentListPage), typeof(ShipmentListPage));
 
             Routing.RegisterRoute(nameof(IncidentFormPage), typeof(IncidentFormPage));
             Routing.RegisterRoute(nameof(IncidentDetailPage), typeof(IncidentDetailPage));
+            Routing.RegisterRoute(nameof(IncidentListPage), typeof(IncidentListPage));
 
+            Routing.RegisterRoute(nameof(UserListPage), typeof(UserListPage));
             Routing.RegisterRoute(nameof(UserProfilePage), typeof(UserProfilePage));
             Routing.RegisterRoute(nameof(ChangePasswordPage), typeof(ChangePasswordPage));
+
             Routing.RegisterRoute(nameof(ResourceAssignPage), typeof(ResourceAssignPage));
+
             Routing.RegisterRoute(nameof(NotificationsPage), typeof(NotificationsPage));
+            
 
 
 
--- ReportesDePaqueteria/MVVM/Views/HomePage.xaml.cs
+++ ReportesDePaqueteria/MVVM/Views/HomePage.xaml.cs
@@ -18,24 +18,34 @@ namespace ReportesDePaqueteria.MVVM.Views
             await _vm.LoadAsync();
         }
 
-        private async void OnShipmentsClicked(object sender, EventArgs e)
-     => await Shell.Current.GoToAsync("//shipments");
-
-        private async void OnIncidentesClicked(object sender, EventArgs e)
-            => await Shell.Current.GoToAsync("//incidents");
-
         private async void OnNavHomeClicked(object sender, EventArgs e)
             => await Shell.Current.GoToAsync("//homePage");
 
+        //Shipment actions
         private async void OnNuevoShipmentClicked(object sender, EventArgs e)
             => await Shell.Current.GoToAsync(nameof(ShipmentFormPage));
 
+        private async void OnShipmentListClicked(object sender, EventArgs e)
+            => await Shell.Current.GoToAsync(nameof(ShipmentListPage));
+
+        private async void OnShipmentDetailClicked(object sender, EventArgs e)
+            => await Shell.Current.GoToAsync(nameof(ShipmentDetailPage));
+
+
+        //Incident actions y noticiaciones
         private async void OnNuevoIncidenteClicked(object sender, EventArgs e)
             => await Shell.Current.GoToAsync(nameof(IncidentFormPage));
 
         private async void OnNotificationsClicked(object sender, EventArgs e)
             => await Shell.Current.GoToAsync(nameof(NotificationsPage));
 
+        private async void OnIncidentListClicked(object sender, EventArgs e)
+            => await Shell.Current.GoToAsync(nameof(IncidentListPage));
+
+        private async void OnIncidentDetailClicked(object sender, EventArgs e)
+            => await Shell.Current.GoToAsync(nameof(IncidentDetailPage));
+
+        //User actions y otros
         private async void OnNavProfileClicked(object sender, EventArgs e)
             => await Shell.Current.GoToAsync(nameof(UserProfilePage));
 
