358456ed22e7b7a3e0e3b40ea37af7d9e4ca5eb5
Tinnged <114170587+Tinnged@users.noreply.github.com>
2025-08-22 22:38:41 -0600

    Cambios menores

--- ReportesDePaqueteria/MVVM/Converters/IsNullConverter.cs
+++ ReportesDePaqueteria/MVVM/Converters/IsNullConverter.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ReportesDePaqueteria.MVVM
+namespace ReportesDePaqueteria.MVVM.Converters
 {
     public class IsNullConverter : IValueConverter
     {
